ef1dcffd251947930b9548ca5a413062da015af4
Sagar Karki <sagar.karki200@gmail.com>
2022-03-19 14:14:21 +0545

    body almost completed

---
+++ src/data/dataBody.js
@@ -0,0 +1,23 @@
+const dataBody = [
+    {
+    id: 0,
+    title: "ghost",
+    button: "discover now",
+    img: "R2.jpg"
+    },
+    {
+    id: 1,
+    title: "bespoke is rolls-royce",
+    button: "discover more",
+    img : "R3.jpg"
+    },
+    {
+    id: 2,
+    title: "inspiring greatness",
+    description: "episode 19: kengo kuma",
+    button: "discover more",
+    img : "R1.jpg"
+    },
+]
+
+export default dataBody
