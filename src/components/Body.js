4b098a4eba5019aaebafa78ba3f86c575fb9ea2a
Sagar Karki <sagar.karki200@gmail.com>
2022-03-19 11:47:48 +0545

    header completed

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Body=()=>{
+    return(
+        <div>
+            Why not?
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components"
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Header =()=>{
+    return ( 
+    <Nav>
+        <Menu>
+            <BurgerMenu/>Menu
+        </Menu>
+        <Logo>
+            <img width="105px" alt="rolls-royce" src="./img/Rolls-Royce-RR.png"/>
+        </Logo>
+        <RightMenu className="right-menu">
+           <Search className="search"/> Find a dealer
+        </RightMenu>
+    </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.div`
+    width: 97vw;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 18px 0;
+    text-transform: uppercase;
+    border-bottom: 0.5px solid black;
+    margin: 10px 17px;
+    position: relative;
+    font-weight:600;
+    font-size: 0.9rem;
+`
+
+const BurgerMenu = styled(MenuIcon)`
+    margin-right: 15px;
+`
+const Menu = styled.div`
+ display: flex;
+ align-items: center;
+ cursor: pointer;
+`
+const Logo =styled.div`
+    cursor: pointer;
+    object-fit: contain;
+`
+const RightMenu = styled.div`
+    display: flex;
+    cursor: pointer;
+   
+`
+
+const Search = styled(SearchIcon)`
+    
+  }
+`
